74c1e925936595105d3090112f7020f56e629fb0
Rohan Chopra <git@rchopra.in>
2022-11-26 10:57:59 -0500

    Add multilabel loaders

---
+++ src/multilabel/data.py
@@ -0,0 +1,67 @@
+import os
+import pandas as pd
+import torchvision.transforms as T
+from torchvision import datasets
+import torch.utils.data as td
+from multilabel.loader import MultiLabelDataset
+
+
+def load_data(path, batch_size, input_size, norm_arr, 
+              num_workers=0):
+    
+    transform_dict = {"train": 
+                          T.Compose([
+                            #T.ToPILImage(),
+                            T.Resize(size=input_size),
+
+                            T.RandomHorizontalFlip(),
+                            #T.ColorJitter(contrast=0.5),
+                            T.RandomAdjustSharpness(2),
+                            T.RandomAutocontrast(),
+
+                            T.ToTensor(),
+                            T.Normalize(*norm_arr)]),
+                        "test_val": 
+                          T.Compose([
+                            #T.ToPILImage(),
+                            T.Resize(size=input_size),
+                            T.ToTensor(),
+                            T.Normalize(*norm_arr)])
+                        }
+    
+    train = pd.read_csv(os.path.join(path, "train.csv")).drop(['Image Index', 'Patient ID'], axis=1).set_index('FilePath')
+    test = pd.read_csv(os.path.join(path, "test.csv")).drop(['Image Index', 'Patient ID'], axis=1).set_index('FilePath')
+    val = pd.read_csv(os.path.join(path, "val.csv")).drop(['Image Index', 'Patient ID'], axis=1).set_index('FilePath')
+    
+    train_dataset = MultiLabelDataset(root=os.path.join(path, "train"),
+                                         dataframe=train,
+                                         transform=transform_dict["train"])
+    val_dataset = MultiLabelDataset(root=os.path.join(path, "val"),
+                                       dataframe=train,
+                                       transform=transform_dict["test_val"])
+    test_dataset = MultiLabelDataset(root=os.path.join(path, "test"),
+                                        dataframe=train,
+                                        transform=transform_dict["test_val"])
+    
+    data_loader_train = td.DataLoader(train_dataset,
+                                      batch_size=batch_size,
+                                      shuffle=True,
+                                      drop_last=False,
+                                      num_workers=num_workers,
+                                      pin_memory=True)
+    data_loader_val = td.DataLoader(val_dataset,
+                                    batch_size=batch_size,
+                                    shuffle=False,
+                                    drop_last=False,
+                                    num_workers=num_workers,
+                                    pin_memory=True)
+    data_loader_test = td.DataLoader(test_dataset,
+                                     batch_size=batch_size,
+                                     shuffle=False,
+                                     drop_last=False,
+                                     num_workers=num_workers,
+                                     pin_memory=True)
+
+    return {'train': data_loader_train,
+            'val':   data_loader_val,
+            'test':  data_loader_test}
---
+++ src/multilabel/loader.py
@@ -0,0 +1,21 @@
+import torch
+
+
+class MultiLabelDataset(Dataset):
+    def __init__(self, root, dataframe, transform = None):
+        self.dataframe = dataframe
+        self.root = root
+        self.transform = transform
+        self.file_names = dataframe.index
+        self.labels = dataframe.values.tolist()
+        
+    def __len__(self):
+        return len(self.dataframe)
+    
+    def __getitem__(self, index):
+        image = Image.open(os.path.join(self.root, self.file_names[index]))
+        label = self.labels[index]
+        if self.transform:
+            image = self.transform(image)
+        
+    return image, torch.tensor(label, dtype=torch.float)
