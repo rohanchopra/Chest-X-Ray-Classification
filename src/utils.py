2f7ff9fa095e7fcb93a698b9a4c5931d533e70d6
Rohan Chopra <git@rchopra.in>
2022-12-03 22:00:44 -0500

    Fix name bug

--- src/utils.py
+++ src/utils.py
@@ -13,15 +13,15 @@ def save_model(model, path, name):
     return True
 
 
-def encode_label(label, classes_list=classes): #encoding the classes into a tensor of shape (11) with 0 and 1s.
-    target = torch.zeros(len(classes))
+def encode_label(label, classes_list): #encoding the classes into a tensor of shape (11) with 0 and 1s.
+    target = torch.zeros(len(classes_list))
     for l in label:
         idx = classes_list.index(l)
         target[idx] = 1
     return target
 
 
-def decode_target(target, threshold=0.5): #decoding the prediction tensors of 0s and 1s into text form
+def decode_target(classes, target, threshold=0.5): #decoding the prediction tensors of 0s and 1s into text form
     result = []
     for i, x in enumerate(target):
         if (x >= threshold):
